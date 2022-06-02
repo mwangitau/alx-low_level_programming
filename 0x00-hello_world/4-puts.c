2b96b31edc7cb5986949c5c16abf8c64192e8e41
John Mwangi <61050075+mwangitau@users.noreply.github.com>
2022-06-02 15:09:09 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,37 +1,19 @@
 /*
- *
- *  * File: 4-puts.c
- *
- *   * Auth: John G Mwangi <mwangijohngitau1.com>
- *
- *    */
-
-
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+ */
 
 #include <stdio.h>
 
-
-
-/*
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- *  * main - Prints "Programming is like building a multilingual
- *
- *  * puzzle, followed by a new line.
- *
- * 
- *
- *  * Return: Always 0.
- *
- *      */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-			return (0);
-
+	return (0);
 }
