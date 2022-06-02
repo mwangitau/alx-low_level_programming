b751a9bd6d55bec0401bdb7d3b167d118abe77b7
mwangitau <mwangijohngitau1@gmail.com>
2022-06-02 06:59:50 -0700

    Edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
 
 /**
  * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ *                puzzle, followed by a new line."
  *
  * Return: Always 0.
  */
