900f9665da1741efa17f06b0643b27f2e096ed84
mwangitau <mwangijohngitau1@gmail.com>
2022-06-03 07:20:32 -0700

    If else statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,11 @@
-/*
- * File: 
- * Auth:
- */
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * Main- Program will assign a random number to the variable n each time it is executed.
- * 	To print whether the number stored in the variable n is positive or negative.
+ * main- Program will assign a random number to the variable n
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int n;
@@ -24,8 +17,7 @@ int main(void)
 		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
-	else 
-		printf("%d is zero\n", n);
-	
+	else
+
 	return (0);
 }
