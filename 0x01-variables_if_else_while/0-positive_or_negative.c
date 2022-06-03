119a664c3c63d6acbc21645bbbf2c747c02661fe
mwangitau <mwangijohngitau1@gmail.com>
2022-06-03 07:35:23 -0700

    The use of if statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main- Program will assign a random number to the variable n
+ * main - Prints a random number and states whether
  *
- * Return: Always 0.
+ *  *        it is positive, negative, or zero.
+ *  Return: Always 0.
  */
 int main(void)
 {
@@ -18,6 +20,8 @@ int main(void)
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
+		printf("%d is zero\n", n);
 
 	return (0);
 }
+
