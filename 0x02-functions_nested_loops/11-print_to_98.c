9c54357a756f865de60343bf086908bc3aab1244
mwangitau <mwangijohngitau1@gmail.com>
2022-06-07 16:45:40 -0700

    function that prints all natural numbers from n to 98, followed by a new line.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,7 @@
 /*
- * File: 11-print_to_98.c
- *
+ * File: 11-print_to_
  */
-#include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - Prints all natural numbers from input to 98,
  *               in order separated by a comma followed by a space.
