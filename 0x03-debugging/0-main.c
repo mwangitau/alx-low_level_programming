e0209f14354427a633459289a284245dbc3b8646
mwangitau <mwangijohngitau1@gmail.com>
2022-06-09 04:40:15 -0700

    file to test that the function positive_or_negative() gives the correct output when given a case of 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,13 @@
  * main -tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 98;
-	positive_or_negative(int i);
+	positive_or_negative(i);
 
 	return (0);
 }
