60d73fa8b76f315374544166375ee7eb3a25a4bd
mwangitau <mwangijohngitau1@gmail.com>
2022-06-28 05:33:42 -0700

    libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: one char type argument
+ *
+ * Description: print a string follow by a new line to stdout
+ * Return: none
+ */
+void _puts(char *str)
+{
+	int l = 0;
+
+	while (*(str + l) != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to check
+ * @accept: string to check against
+ *
+ * Return: number of bytes of s in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x, y;
+
+	for (x = 0; s[x]; x++)
+	{
+		for (y = 0; accept[y]; y++)
+		{
+			if (s[x] == accept[y])
+				break;
+		}
+		if (!accept[y])
+			break;
+	}
+	return (x);
+}
