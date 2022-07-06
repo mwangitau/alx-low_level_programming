d21f16a9da906eeb96e462d05d7e782d40ad48c0
mwangitau <mwangijohngitau1@gmail.com>
2022-07-06 01:24:40 -0700

    Define a new Struct with aome elements

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+
+/**
+ * init_dog - inintializes a structure of type dog
+ * @d: pointer to structure
+ * @name: pointer to the name of the dog
+ * @age: age of the dog
+ * @owner: pointer to owner of dog
+ *
+ * Return: void
+ */
+
