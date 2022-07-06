76e1ba6761e8c5c07d2a86b06fe3e3c59fc537af
mwangitau <mwangijohngitau1@gmail.com>
2022-07-06 10:34:09 -0700

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,38 +11,38 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int x = 0, y = 0, z;
-	dog_t *cigi;
+	int i = 0, j = 0, k;
+	dog_t *doge;
 
-	while (name[x] != '\0')
-		x++;
-	while (owner[y] != '\0')
-		y++;
-	cigi = malloc(sizeof(dog_t));
-	if (cigi == NULL)
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	doge = malloc(sizeof(dog_t));
+	if (doge == NULL)
 	{
-		free(cigi);
+		free(doge);
 		return (NULL);
 	}
-	cigi->name = malloc(x * sizeof(cigi->name));
-	if (cigi->name == NULL)
+	doge->name = malloc(i * sizeof(doge->name));
+	if (doge->name == NULL)
 	{
-		free(cigi->name);
-		free(cigi);
+		free(doge->name);
+		free(doge);
 		return (NULL);
 	}
-	for (z = 0; z <= x; z++)
-		cigi->name[z] = name[z];
-	cigi->age = age;
-	cigi->owner = malloc(y * sizeof(cigi->owner));
-	if (cigi->owner == NULL)
+	for (k = 0; k <= i; k++)
+		doge->name[k] = name[k];
+	doge->age = age;
+	doge->owner = malloc(j * sizeof(doge->owner));
+	if (doge->owner == NULL)
 	{
-		free(cigi->owner);
-		free(cigi->name);
-		free(cigi);
+		free(doge->owner);
+		free(doge->name);
+		free(doge);
 		return (NULL);
 	}
-	for (z = o; z <= y; z++)
-		cigi->owner[z] = owner[z];
-	return (cigi);
+	for (k = 0; k <= j; k++)
+		doge->owner[k] = owner[k];
+	return (doge);
 }
