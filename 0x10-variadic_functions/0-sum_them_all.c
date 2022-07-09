474799fa0afb8f9d35f301666106e90bbb021fd6
mwangitau <mwangijohngitau1@gmail.com>
2022-07-09 02:58:04 -0700

    function that returns the sum of all its parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - sums of all its parameters.
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+
 #include <stdio.h>
 #include <stdarg.h>
 
